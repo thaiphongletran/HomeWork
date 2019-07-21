3c1ccfabd6c23eda95775c06c061ca375a581ea2
thaiphongletran <52129250+thaiphongletran@users.noreply.github.com>
2019-07-21 08:50:48 +0700

    Delete quizz.js

--- quizz.js
+++
@@ -1,7 +0,0 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('maincontroller',['$scope', '$http', function($scope, $http){
-    var url = "ADAV.js";
-    $http.get(url).success(function(response) {
-        $scope.ADAV = response;
-    });
-}]);
