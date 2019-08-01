e771c7bcd1f4cbc02f06659cc4233310266c10b1
LeTranThaiPhong <thaiphongletran@gmail.com>
2019-08-01 08:47:41 +0700

    HomeWork

---
+++ coreui-free-angular-admin-template/src/app/_nav.ts
@@ -0,0 +1,65 @@
+interface NavAttributes {
+  [propName: string]: any;
+}
+interface NavWrapper {
+  attributes: NavAttributes;
+  element: string;
+}
+interface NavBadge {
+  text: string;
+  variant: string;
+}
+interface NavLabel {
+  class?: string;
+  variant: string;
+}
+
+export interface NavData {
+  name?: string;
+  url?: string;
+  icon?: string;
+  badge?: NavBadge;
+  title?: boolean;
+  children?: NavData[];
+  variant?: string;
+  attributes?: NavAttributes;
+  divider?: boolean;
+  class?: string;
+  label?: NavLabel;
+  wrapper?: NavWrapper;
+}
+
+export const navItems: NavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  
+  {
+    name: 'Danh sách học sinh',
+    url: '/base',
+    icon: 'icon-puzzle'
+  },
+  {
+    name: 'Pages',
+    url: '/pages',
+    icon: 'icon-star',
+    children: [
+      {
+        name: 'Login',
+        url: '/login',
+        icon: 'icon-star'
+      },
+      {
+        name: 'Register',
+        url: '/register',
+        icon: 'icon-star'
+      }
+    ]
+  },
+];
